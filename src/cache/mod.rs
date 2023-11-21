72469b8f52a947384029604966bf0d8faee2e8e3
Subhradeep <chakrabortysubhradeep556@gmail.com>
2023-11-21 16:38:02 +0530

    Set up cache interface

---
+++ src/cache/mod.rs
@@ -0,0 +1,19 @@
+use std::hash::Hash;
+
+pub trait Cache<K, V>
+where
+    K: Eq + Hash,
+{
+    /// Instantiates a new Cache
+    fn new(capacity: usize) -> Self;
+
+    /// Puts a new key-value pair into the cache
+    fn put(&mut self, key: K, value: V) -> Result<(), String>;
+
+    /// Returns the stored value against the given key
+    fn get(&self, key: &K) -> Option<&V>;
+
+    /// When the Cache capacity is filled, this function removes key-value pair
+    /// based on different policies.
+    fn evact(&self, key: K) -> Result<(), String>;
+}
