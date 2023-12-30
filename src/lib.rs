50d9c16dd1c87c6c3ede4658d57948853becbe03
Subhradeep <chakrabortysubhradeep556@gmail.com>
2023-12-30 21:17:46 +0530

    Implement grpc server methods for CacheServer

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,9 @@
+use cache::Cache;
 use network::CacheNetwork;
+use rpc::{Entry, GetResponse, Key, PutResponse};
 use std::marker::PhantomData;
+use tokio::sync::Mutex;
+use tonic::{Request, Response, Result, Status};
 
 pub mod rpc {
     tonic::include_proto!("api");
@@ -46,11 +50,56 @@ impl CacheClusterServer {
 impl CacheClusterServer<HTTPServer> {}
 
 // RPC server for the Cache
-pub struct CacheServer<T = RPCServer>
+pub struct CacheServer<C, T = RPCServer>
 where
+    C: Cache<String, String>,
     T: Server,
 {
+    cache: Mutex<C>,
     pd: PhantomData<T>,
 }
 
-// impl<K, V> for CacheServer<K, V> {}
+impl<C, T> CacheServer<C, T>
+where
+    C: Cache<String, String> + Send + 'static,
+    T: Server,
+{
+    pub fn new(cache: C) -> Self {
+        CacheServer::<C, T> {
+            cache: Mutex::new(cache),
+            pd: PhantomData,
+        }
+    }
+}
+
+#[tonic::async_trait]
+impl<C> rpc::cache_server::Cache for CacheServer<C>
+where
+    C: Cache<String, String> + Send + 'static,
+{
+    async fn get(&self, request: Request<Key>) -> Result<Response<GetResponse>> {
+        let key = request.into_inner().key;
+
+        let mut cache = self.cache.lock().await;
+        match cache.get(&key) {
+            Some(value) => Ok(Response::new(GetResponse {
+                code: 0,
+                value: value.clone(),
+                message: "success".to_string(),
+            })),
+            None => Err(Status::not_found("key not found")),
+        }
+    }
+
+    async fn put(&self, request: Request<Entry>) -> Result<Response<PutResponse>> {
+        let Entry { key, value } = request.into_inner();
+        let mut cache = self.cache.lock().await;
+        match cache.put(key, value) {
+            Ok(()) => Ok(Response::new(PutResponse {
+                code: 0,
+                message: "success".to_string(),
+            })),
+            Err(msg) => Err(Status::internal(msg)),
+        }
+    }
+}
