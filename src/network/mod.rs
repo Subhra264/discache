2aa50b191a9cfc59fc4eb746cbf712ee7a46935f
Subhradeep <chakrabortysubhradeep556@gmail.com>
2024-01-01 13:39:49 +0530

    Add network::Error and ServerNode::parse() method

--- src/network/mod.rs
+++ src/network/mod.rs
@@ -1,3 +1,20 @@
+use std::net::{SocketAddr, ToSocketAddrs};
+
+#[derive(Debug)]
+pub enum Error {
+    NotValidAddress,
+}
+
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Error::NotValidAddress => f.write_str("given address not valid"),
+        }
+    }
+}
+
+impl std::error::Error for Error {}
+
 pub struct CacheNetwork {
     nodes: Vec<ServerNode>,
 }
@@ -25,6 +42,26 @@ pub struct ServerNode {
 impl ServerNode {
     pub fn new(host: String, port: u16, weight: usize) -> Self {
         ServerNode { host, port, weight }
+    }
+
+    pub fn parse(addr: &str, weight: usize) -> Result<Self, Error> {
+        if let Ok(socket_addr) = addr.to_socket_addrs() {
+            match socket_addr.collect::<Vec<_>>().first() {
+                Some(SocketAddr::V4(v4)) => Ok(ServerNode {
+                    host: v4.ip().to_string(),
+                    port: v4.port(),
+                    weight,
+                }),
+                Some(SocketAddr::V6(v6)) => Ok(ServerNode {
+                    host: v6.ip().to_string(),
+                    port: v6.port(),
+                    weight,
+                }),
+                None => Err(Error::NotValidAddress),
+            }
+        } else {
+            Err(Error::NotValidAddress)
+        }
     }
 
     pub fn get_host(&self) -> String {
